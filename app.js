90c18b12e8889d014e2d0393177a646a41b86efa
maksumrifai <maksum.rifai@gmail.com>
2020-11-27 18:29:11 +0700

    update

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-var study = new Vue({
-  el: "#vue-app",
-  data: {
-    heading: "Vue.js + Bootstrap",
-    text: "As a computer, I find your faith in technology amusing."
-  }
-});
-
 Sentry.init({
   dsn: "https://04e54dc742284eb7851840e977798e12@o483326.ingest.sentry.io/5534852",
   integrations: [
