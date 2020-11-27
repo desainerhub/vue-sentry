b05157703784429ef86302ba66272746bc78bc5e
maksumrifai <maksum.rifai@gmail.com>
2020-11-27 18:19:26 +0700

    initial commit

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ var study = new Vue({
     text: "As a computer, I find your faith in technology amusing."
   }
 });
+
+Sentry.init({
+  dsn: "https://04e54dc742284eb7851840e977798e12@o483326.ingest.sentry.io/5534852",
+  integrations: [
+    new Sentry.Integrations.Vue({
+      Vue,
+      tracing: true,
+    }),
+    new Sentry.Integrations.BrowserTracing(),
+  ],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
